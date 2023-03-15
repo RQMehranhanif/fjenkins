ef0e3b45c6228350ae6854deff7f3677aef656d8
RQMehranhanif <mehran.hanif@vitalanalytics.net>
2023-03-15 08:06:03 +0500

    new file:   Dockerfile 	new file:   docker-compose.yml 	modified:   package.json 	new file:   react-js-deployment.yml 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          hahhahhashhahhahhahah ma ho krn mari marzi new code agya 
+         Build is Runing Succesfully
         </p>
       </header>
     </div>
